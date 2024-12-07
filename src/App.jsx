1f2e5f7eb1f6b3df17e4f94341671f893cdaf6c2
anjana732 <anjanasingh1257@gmail.com>
2024-12-07 23:58:26 +0530

    Add Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,70 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [character, setCharacter] = useState(false);
+  const [number, setNumber] = useState(false);
+  const [password, setPassword] = useState("");
+  const passRef = useRef(null);
+
+  const passwordGenerator = useCallback(()=>{
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(number) str += "0123456789";
+    if(character) str+= "~!@#$%^&*()_-+=[]{}?/<>,."
+
+    for (let i = 1; i <= length; i++) {
+      const char = Math.floor(Math.random()*str.length + 1)     
+      pass += str.charAt(char) 
+    }
+    setPassword(pass);
+  },[length,character,number,setPassword])
+
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,character,number,passwordGenerator])
+
+  useRef
+
+  const copyPassword = useCallback(() => {
+    passRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={{backgroundColor: "gray"}}> 
+      <h1>Password Generator</h1>
+      <input 
+      type='text'
+      value={password}
+      readOnly
+      ref={passRef}
+      ></input>
+      <button onClick={copyPassword}>Copy</button><br></br>
+      <input 
+      type="range"
+      min={0}
+      max={100} 
+      value={length}
+      onChange={(e)=>{setLength(e.target.value)}}/>
+      <label>Length : {length}</label>
+      <input type="checkbox" 
+      defaultChecked={number}
+      onChange={()=>{
+        setNumber((prev)=> !prev)
+      }}/>
+      <label>Number</label>
+      <input type='checkbox'
+      defaultChecked={character}
+      onChange={()=>{
+        setCharacter((prev)=> !prev)
+      }}/>
+      <label>Character</label>
+    </div> 
     </>
   )
 }
